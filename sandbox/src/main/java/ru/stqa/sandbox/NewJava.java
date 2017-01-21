2c3b8aa3b46d114d7080890ccfbc6c478388d102
max2109 <alexey2109@gmail.com>
2017-01-20 23:03:41 -0800

    Calculations

--- sandbox/src/main/java/ru/stqa/sandbox/NewJava.java
+++ sandbox/src/main/java/ru/stqa/sandbox/NewJava.java
@@ -2,7 +2,25 @@ package ru.stqa.sandbox;
 
 public class NewJava {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2/2);
+    System.out.println(2-2);
+
+    System.out.println(1/2);
+    System.out.println(1.0/2);
+    System.out.println(1/2.0);
+    System.out.println(2.0/2);
+
+    System.out.println("2"+"2");
+    System.out.println("2"+2);
+    System.out.println(2+"2");
+
+    System.out.println((2+2) * 2);
+
+    System.out.println("2+2 = " +(2 +2) );
+  }
+
 }
