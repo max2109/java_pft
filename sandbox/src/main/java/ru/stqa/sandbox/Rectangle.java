b437a32f0016fa7c4ca54bda0b318dfe852975e8
max2109 <alexey2109@gmail.com>
2017-01-21 00:21:47 -0800

    New Class and Objects

---
+++ sandbox/src/main/java/ru/stqa/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by alexe on 1/21/2017.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by alexe on 1/21/2017.
+ */
+public class Square {
+
+  public double l;
+  
+  public Square(double l){
+    this.l = l;
+  }
+}
