94351a92da87a997f8b05c1848fd3bee3bb8375c
max2109 <alexey2109@gmail.com>
2017-01-20 23:19:50 -0800

    New calculation changes

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -7,8 +7,12 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, " + "world");
+
+    int l = 8;
+    int s = l*l;
+    System.out.println("Площадь квадрата со стороной " +l+ "=" + s);
   }
-  
+
 }
 
 
