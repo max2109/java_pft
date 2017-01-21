2fae0140d8cdada9d1b84e426196e5c9e939560e
max2109 <alexey2109@gmail.com>
2017-01-20 23:13:44 -0800

    New program

---
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -0,0 +1,16 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by alexe on 1/20/2017.
+ */
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, " + "world");
+  }
+  
+}
+
+
+
+
