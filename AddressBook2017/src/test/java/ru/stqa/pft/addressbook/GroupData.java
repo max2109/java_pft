23e44c0d88ca006cc23c555f30a4e85b45e125fe
max2109 <alexey2109@gmail.com>
2017-11-01 13:08:00 -0700

    Выделение вспомогательных функций
    создание объекта с параметрами для метода

---
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+    private final String name; //аттрибуты
+    private final String header; //аттрибуты
+    private final String footer; //аттрибуты
+
+    public GroupData(String name, String header, String footer) { //конструктор
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getName() {
+        return name;
+    } //методы возвращающие аттрибуты
+
+    public String getHeader() {
+        return header;
+    } //методы возвращающие аттрибуты
+
+    public String getFooter() {
+        return footer;
+    } //методы возвращающие аттрибуты
+}
