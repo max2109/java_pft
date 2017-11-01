1dec6fcf86c227f09bb18884ec015885ecec8842
max2109 <alexey2109@gmail.com>
2017-11-01 16:02:32 -0700

    Browsers Added

--- AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/TestBase.java
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/TestBase.java
@@ -3,10 +3,12 @@ package ru.stqa.pft.addressbook.Tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+
 
 public class TestBase { //изчезает extends после делегирования
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
     //и создается ссылка на новый объект ApplicationManager
 
 
