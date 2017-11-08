2c6027f82f24d5341957a104e7ebeb662bfd1be5
max2109 <alexey2109@gmail.com>
2017-11-07 16:04:51 -0800

    new commit

---
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/ContactPhoneTest.java
@@ -0,0 +1,14 @@
+//package ru.stqa.pft.addressbook.Tests;
+//
+//import org.testng.annotations.Test;
+//import ru.stqa.pft.addressbook.model.ContactData;
+//
+//public class ContactPhoneTest extends TestBase{
+//
+//    @Test (enabled = false)
+//    public void testContactPhone (){
+//        app.goTo().gotoHomePage();
+//        ContactData contact = app.contact.all().iterator().next();
+//        ContactData contactInfoFromEditForm = app.contact().info
+//    }
+//}
