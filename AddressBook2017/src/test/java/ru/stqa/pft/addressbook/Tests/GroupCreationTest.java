218c1e524ce602fe106f30ed82e6e0f98fadbd9b
max2109 <alexey2109@gmail.com>
2017-11-16 14:11:31 -0800

    Deleted comments
    Deleted comments

--- AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
@@ -22,30 +22,7 @@ public class GroupCreationTest extends TestBase {
         //List<GroupData> after = app.group().list();
         assertThat(app.group().count(), equalTo(before.size()+1));
         Groups after = app.group().all();//Set<GroupData> after = app.group().all();
-        //int after = app.getGroupHelper().count();
-      //  Assert.assertEquals(after, before +1); // проверка количества групп после добавления
 
-
-        //старый способ сравнения
-//        int max = 0;
-//        for (GroupData g : after){  //цикл чтобы определить максимальный идентификатор
-//            if (g.getId() > max){
-//                max = g.getId();
-//            }
-//        }
-        //новый способ сравнения при помощи сравнивателя Comparator преобразованного в Лямбду
-
-      //  group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-       // group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()); //function mapToInt преобразует множество в число
-                                                            //проще написать функцию которая преобразует идетификатор в число, чем
-                                                            //написать функцию которая сравнивает два объекта
-        //before.add(group);
-//        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-//        before.sort(byId);
-//        after.sort(byId);
-       // Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after)); //для сравние нужно преобразовать списки в множества и сравнивать без учета порядка
-
-      //  assertEquals(before, after);
         assertThat(after, equalTo(before.withAdded(
                 group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))); //add static import
     }
