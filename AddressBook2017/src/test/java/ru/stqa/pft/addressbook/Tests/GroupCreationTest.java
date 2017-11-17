936473454ff22b01fe43f1b5e559119a9a9fb328
max2109 <alexey2109@gmail.com>
2017-11-17 01:20:21 -0800

    Added reading testData from XML Files

--- AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
@@ -1,8 +1,19 @@
 package ru.stqa.pft.addressbook.Tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -11,21 +22,33 @@ import static org.testng.Assert.assertEquals;
 public class GroupCreationTest extends TestBase {
     //после наследования от класса TestBase --> pull members up --> переносит методы в TestBase
 
-    @Test
-    public void testGroupCreation() {
-        app.goTo().groupPage(); //extract methods
-        //List<GroupData> before = app.group().list();
-        Groups before = app.group().all(); // Set<GroupData> before = app.group().all();
-        //int before = app.getGroupHelper().count();
-        GroupData group = new GroupData().withName("test2");
-        app.group().create(group);
-        //List<GroupData> after = app.group().list();
-        assertThat(app.group().count(), equalTo(before.size()+1));
-        Groups after = app.group().all();//Set<GroupData> after = app.group().all();
-
-        assertThat(after, equalTo(before.withAdded(
-                group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))); //add static import
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
     @Test
     public void testBadGroupCreation() {
         app.goTo().groupPage();
