0ecb22415199e1e3d37e6792e6100e3fe9af1c2d
max2109 <alexey2109@gmail.com>
2017-11-21 15:27:39 -0800

    Modified build.Gradle +created testng-contatcs/groups.xml
    To run test in order

--- AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupDeletionTest.java
+++ AddressBook2017/src/test/java/ru/stqa/pft/addressbook/Tests/GroupDeletionTest.java
@@ -1,16 +1,26 @@
 package ru.stqa.pft.addressbook.Tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.Tests.TestBase;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -22,6 +32,22 @@ public class GroupDeletionTest extends TestBase {
         app.goTo().groupPage();
         if (app.group().all().size() == 0)  {
             app.group().create(new GroupData().withName("test1"));
+        }
+    }
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+            }.getType());
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
     }
 
